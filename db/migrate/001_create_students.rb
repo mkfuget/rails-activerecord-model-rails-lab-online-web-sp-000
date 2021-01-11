5f8870be0f1d459609ffdd0d9d95338b965d40ad
Michael Fuget <michaelfuget@gmail.com>
2021-01-11 06:49:32 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_students.rb
+++ db/migrate/001_create_students.rb
@@ -2,7 +2,11 @@ class CreateStudents < ActiveRecord::Migration
   def change
     create_table :students do |t|
       t.string :first_name
+<<<<<<< HEAD
       t.string :last_name
+=======
+      t.string :second_name
+>>>>>>> d8aa9a357af4314263471387c935a6b181f9bb63
       t.timestamps null: false
     end
   end
